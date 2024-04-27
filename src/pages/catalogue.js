e9de326ff780f38a4a230232f1f315c0d60679db
Petar Patev <petarpatev@abv.bg>
2024-04-27 10:51:49 +0300

    Add catalogue page

--- src/pages/catalogue.js
+++ src/pages/catalogue.js
@@ -1,7 +1,38 @@
-
-
 const catalogueElement = document.querySelector('.catalogue');
 
+const url = 'http://localhost:3030/data/recipes';
+
 export function renderCatalogue() {
-    catalogueElement.style.display = 'block';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(recipes => {
+            addAllRecipes(recipes);
+            catalogueElement.style.display = 'block';
+        })
+        .catch(err => console.log(err))
+}
+
+function addAllRecipes(recipes) {
+    const recipesFragment = document.createDocumentFragment();
+    recipes.forEach(r => {
+        recipesFragment.appendChild(createRecipe(r));
+    })
+    catalogueElement.innerHTML = '';
+    catalogueElement.appendChild(recipesFragment)
+}
+
+function createRecipe(recipe) {
+    let recipeContainer = document.createElement('article');
+    recipeContainer.classList.add('preview');
+    recipeContainer.innerHTML = `
+                <div class="title">
+                    <h2>${recipe.name}</h2>
+                </div>
+                <div class="small">
+                    <img src=${recipe.img}>
+                </div>
+                `
+
+    return recipeContainer;
 }
