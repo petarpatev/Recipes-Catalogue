adf69b2fb9aa015c314835b282c12e363cab6318
Petar Patev <petarpatev@abv.bg>
2024-04-28 10:04:12 +0300

    Add create recipe functionality

--- src/pages/create.js
+++ src/pages/create.js
@@ -5,3 +5,37 @@ const createElement = document.querySelector('.create');
 export function renderCreate() {
     createElement.style.display = 'block';
 }
+
+const url = 'http://localhost:3030/data/recipes';
+const createFormElement = createElement.querySelector('form');
+createFormElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.currentTarget);
+    let name = formData.get('name');
+    let img = formData.get('img');
+    let ingredients = formData.get('ingredients').split('\n');
+    let steps = formData.get('steps').split('\n');
+
+    let newRecipe = {
+        name,
+        img,
+        ingredients,
+        steps,
+    }
+
+    const token = JSON.parse(localStorage.getItem('user')).accessToken;
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'X-authorization': token
+        },
+        body: JSON.stringify(newRecipe)
+    })
+        .then(response => response.json())
+        .then(newRecipe => {
+            createFormElement.reset();
+        })
+        .catch(err => console.log(err))
+})
